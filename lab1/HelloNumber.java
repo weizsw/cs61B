18db3936967691492d52e2b32522fc0539e16403
weizsw <weizsw888@gmail.com>
2020-06-01 17:47:01 -0400

    updated lab1 and lab2

---
+++ lab1/HelloNumber.java
@@ -0,0 +1,11 @@
+public class HelloNumber {
+    public static void main(String[] args) {
+        int x;
+        x = 0;
+        while (x < 10) {
+            System.out.println(x);
+            x++;
+        }
+    }
+}
+
--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,25 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A == null) {
+            return B;
+        }
+
+        if (B == null) {
+            return A;
+        }
+
+        if ((A == null) && (B == null)) {
+            return null;
+        }
+
+        IntList temp = A;
+        while (temp.rest != null) {
+            temp = temp.rest;
+        }
+        temp.rest = B;
+
+        return A;
     }
 
     /**
@@ -91,7 +109,52 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A == null) {
+            IntList res = new IntList(B.first, null);
+            IntList ptr = res;
+            B = B.rest;
+            while (B != null) {
+                ptr.rest = new IntList(B.first, null);
+                B = B.rest;
+                ptr= ptr.rest;
+            }
+            return res;
+        }
+
+        if (B == null) {
+            IntList res = new IntList(A.first, null);
+            IntList ptr = res;
+            A = A.rest;
+
+            while (A != null) {
+                ptr.rest = new IntList(A.first, null);
+                A = A.rest;
+                ptr = ptr.rest;
+            }
+
+            return res;
+        }
+
+        if (A == null && B == null) {
+            return null;
+        }
+
+        IntList res = new IntList(A.first, null);
+        IntList ptr = res;
+        A = A.rest;
+
+        while (A != null) {
+            ptr.rest = new IntList(A.first, null);
+            A = A.rest;
+            ptr = ptr.rest;
+        }
+        while (B != null) {
+            ptr.rest = new IntList(B.first, null);
+            B = B.rest;
+            ptr= ptr.rest;
+        }
+
+        return res;
     }
 
 
