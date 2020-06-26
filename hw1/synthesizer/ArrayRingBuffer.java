c10da845afb96d1eb3fb86b789eaf76615f16930
weizsw <weizsw888@gmail.com>
2020-06-26 19:01:00 -0400

    HW1 finished

---
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -0,0 +1,107 @@
+package synthesizer;
+import java.util.Iterator;
+
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
+    /* Index for the next dequeue or peek. */
+    private int first;            // index for the next dequeue or peek
+    /* Index for the next enqueue. */
+    private int last;
+    /* Array for storing the buffer data. */
+    private T[] rb;
+
+    /**
+     * Create a new ArrayRingBuffer with the given capacity.
+     */
+    public ArrayRingBuffer(int capacity) {
+
+        rb = (T[]) new Object[capacity];
+        this.capacity = capacity;
+        fillCount = 0;
+        first = 0;
+        last = first;
+
+
+    }
+
+    /**
+     * Adds x to the end of the ring buffer. If there is no room, then
+     * throw new RuntimeException("Ring buffer overflow"). Exceptions
+     * covered Monday.
+     */
+    @Override
+    public void enqueue(T x) {
+        rb[last] = x;
+        if (isFull()) {
+            throw new RuntimeException("Ring buffer overflow");
+        } else {
+            fillCount += 1;
+        }
+        if (last == capacity - 1) {
+            last = 0;
+        } else {
+            last += 1;
+        }
+    }
+
+    /**
+     * Dequeue oldest item in the ring buffer. If the buffer is empty, then
+     * throw new RuntimeException("Ring buffer underflow"). Exceptions
+     * covered Monday.
+     */
+    @Override
+    public T dequeue() {
+
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        } else {
+            T removed = rb[first];
+            if (first != capacity - 1) {
+                first += 1;
+            } else {
+                first = 0;
+            }
+            fillCount -= 1;
+            return removed;
+        }
+    }
+
+    /**
+     * Return oldest item, but don't remove it.
+     */
+    @Override
+    public T peek() {
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+        return rb[first];
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new ArrayRingBufferIterator();
+    }
+
+    class ArrayRingBufferIterator implements Iterator<T> {
+        private int cur;
+        private int count;
+        ArrayRingBufferIterator() {
+            cur = first;
+            count = fillCount();
+        }
+        @Override
+        public boolean hasNext() {
+            return count < capacity();
+        }
+        @Override
+        public T next() {
+            T nextItem = rb[cur];
+            if (cur == capacity() - 1) {
+                cur = 0;
+            } else {
+                cur += 1;
+            }
+            return nextItem;
+        }
+    }
+
+}
---
+++ hw1/synthesizer/GuitarString.java
@@ -0,0 +1,46 @@
+package synthesizer;
+
+//Make sure this class is public
+public class GuitarString {
+    /** Constants. Do not change. In case you're curious, the keyword final means
+     * the values cannot be changed at runtime. We'll discuss this and other topics
+     * in lecture on Friday. */
+    private static final int SR = 44100;      // Sampling Rate
+    private static final double DECAY = .996; // energy decay factor
+
+    /* Buffer for storing sound data. */
+    private BoundedQueue<Double> buffer;
+
+    /* Create a guitar string of the given frequency.  */
+    public GuitarString(double frequency) {
+        buffer = new ArrayRingBuffer<Double>((int) (Math.round(SR / frequency)));
+        for (int i = 0; i < buffer.capacity(); i++) {
+            buffer.enqueue(0.0);
+        }
+    }
+
+
+    /* Pluck the guitar string by replacing the buffer with white noise. */
+    public void pluck() {
+        for (int i = 0; i < buffer.capacity(); i++) {
+            buffer.dequeue();
+            double r = Math.random() - 0.5;
+            buffer.enqueue(r);
+        }
+    }
+
+    /* Advance the simulation one time step by performing one iteration of
+     * the Karplus-Strong algorithm. 
+     */
+    public void tic() {
+        double first = buffer.dequeue();
+        double second = buffer.peek();
+        double res = (first + second) * 0.5 * DECAY;
+        buffer.enqueue(res);
+    }
+
+    /* Return the double at the front of the buffer. */
+    public double sample() {
+        return buffer.peek();
+    }
+}
