5d15d4fda867bc1a1f01a2ea4a462e54964b5971
weizsw <weizsw888@gmail.com>
2020-07-08 19:09:01 -0400

    hw2 finished

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,80 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private boolean[][] grid;
+    private int N;
+    private int[][] dirs = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    private WeightedQuickUnionUF setWithTopnBottom;
+    private WeightedQuickUnionUF withoutBottom;
+    private int top;
+    private int bottom;
+    private int size;
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        setWithTopnBottom = new WeightedQuickUnionUF(N * N + 2);
+        withoutBottom = new WeightedQuickUnionUF(N * N + 1);
+        top = 0;
+        bottom = N * N + 1;
+        grid = new boolean[N][N];
+        this.N = N;
+        size = 0;
+    }
+
+    private int xyTo1D(int row, int col) {
+        return row * N + col + 1;
+    }
+
+    private void validate(int row, int col) {
+        if (N <= row || row < 0 || col < 0 || col >= N) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+    }
+    public void open(int row, int col) {
+        validate(row, col);
+        if (!isOpen(row, col)) {
+            grid[row][col] = true;
+            size++;
+        }
+        if (row == 0) {
+            setWithTopnBottom.union(xyTo1D(row, col), top);
+            withoutBottom.union(xyTo1D(row, col), top);
+        }
+
+        if (row == N - 1) {
+            setWithTopnBottom.union(xyTo1D(row, col), bottom);
+        }
+
+        for (int[] d : dirs) {
+            int x = d[0] + row;
+            int y = d[1] + col;
+            if (x >= 0 && x < N && y >= 0 && y < N) {
+                if (isOpen(x, y)) {
+                    setWithTopnBottom.union(xyTo1D(row, col), xyTo1D(x, y));
+                    withoutBottom.union(xyTo1D(row, col), xyTo1D(x, y));
+                }
+            }
+        }
+    }
+
+    public boolean isOpen(int row, int col) {
+        validate(row, col);
+        return grid[row][col];
+    }
+
+    public boolean isFull(int row, int col) {
+        validate(row, col);
+        return withoutBottom.connected(xyTo1D(row, col), top);
+
+    }
+
+    public int numberOfOpenSites() {
+        return size;
+    }
+
+    public boolean percolates() {
+        return setWithTopnBottom.connected(top, bottom);
+    }
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,52 @@
 package hw2;
 
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+
 public class PercolationStats {
+    private int times;
+    private double[] fractions;
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        times = T;
+        fractions = new double[T];
+        int totalsites = N * N;
+        for (int i = 0; i < N; i++) {
+            int numOpenedSites = 0;
+            Percolation test = pf.make(N);
+            while (!test.percolates()) {
+                int x = StdRandom.uniform(N);
+                int y = StdRandom.uniform(N);
+                if (!test.isOpen(x, y)) {
+                    test.open(x, y);
+                    numOpenedSites++;
+                }
+            }
+            fractions[i] = numOpenedSites / totalsites;
+        }
+
+    }
+
+    public double mean() {
+        return StdStats.mean(fractions);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(fractions);
+    }
+
+    public double confidenceLow() {
+        double mean = mean();
+        double std = stddev();
+        return mean - (1.96 * std / Math.sqrt(times));
+    }
+
+    public double confidenceHigh() {
+        double mean = mean();
+        double std = stddev();
+        return mean + (1.96 * std / Math.sqrt(times));
+    }
 
 }
