64cdaab8eee23fd4a220cb488e5e2f9eca337119
weizsw <weizsw888@gmail.com>
2020-07-09 15:42:05 -0400

    hw2 finished

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -22,7 +22,7 @@ public class PercolationStats {
                     test.open(x, y);
                 }
             }
-            fractions[i] = (double)test.numberOfOpenSites() / totalSites;
+            fractions[i] = (double) test.numberOfOpenSites() / totalSites;
         }
 
     }
